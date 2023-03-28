a91b483e3eb97c881f05e6d4c19dd541394f5ab2
Charger X33 <66776191+Chargerx33@users.noreply.github.com>
2023-03-28 08:59:41 +0200

    Initial Import

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+
+namespace PasswortGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Wie lang soll das Passwort sein?: ");
+            int lang = Convert.ToInt32(Console.ReadLine());
+            generaror gen = new generaror();
+            gen.asciiPasswort(lang);
+            gen.HexPasswort(lang);
+
+            
+            Console.ReadLine();
+        }
+    }
+    public class generaror
+    {
+        public void asciiPasswort(int Laenge)
+        {
+            const string ascii = "0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ!§$%&/()=?#*+~-_@€";
+            char[] chars = ascii.ToCharArray();
+            Random random = new Random();
+            string password = "";
+            for (int i = 0; i < Laenge; i++)
+            {
+                password = $"{password}{char.ToString(chars[random.Next(chars.Length)])}";
+            }
+            Console.WriteLine("Ascii Passwort: " + password);
+
+        }
+        public void HexPasswort(int Laenge)
+        {
+            const string Hex = "0123456789ABCDEF";
+            char[] chars = Hex.ToCharArray();
+            Random random = new Random();
+            string password = "";
+            for (int i = 0; i < Laenge; i++)
+            {
+                password = $"{password}{char.ToString(chars[random.Next(chars.Length)])}";
+            }
+            Console.WriteLine("Hex Passwort: " + password);
+
+        }
+    }
+}
